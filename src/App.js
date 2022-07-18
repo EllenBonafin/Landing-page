7acf2ecddbc4f2e52faac573df4f5c1f190b1c07
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-07-18 14:10:26 -0300

    PARTII

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="main">
         <div className="center">
-          <div className="menu">
+         <div className="menu">
             <div className="logo">
-              <h3>HELLO</h3>
+                <h3>.codes</h3>
             </div>
             <div className="Item-Menu">
-              <a href="# "> login</a>
+              <a href="# " > login</a>
             </div>
-          </div>
-        </div>
+         </div>
+         <div className='form'>
+          <h2>Entre em Contato</h2>
+            <form>
+                <div className='itens-form'>
+                  <input type="text"/>
+                  <input type="text"/>
+                  <input type="text"/>
+                  <input type="text"/>
+                  <input type="submit"/>
+                </div>
+            </form>
+         </div>
+      </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
