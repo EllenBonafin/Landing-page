0df707f2fb24dae82a7bf2bf1932f229ca410ee4
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-07-18 13:40:15 -0300

    PARTI

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+              <h3>HELLO</h3>
+            </div>
+            <div className="Item-Menu">
+              <a href="# "> login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
